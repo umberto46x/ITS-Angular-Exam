fd1eede0a15efc6524640b31223e700634038a16
umberto46x <daniele.u.nicosia.dev@gmail.com>
2025-06-15 17:28:47 +0200

    [fix] improved UX for mobile users { navbar }

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -13,19 +13,42 @@ import { CommonModule } from '@angular/common';
         <a class="navbar-brand d-flex align-items-center" routerLink="/">
           <span class="fw-bold fs-3">🍕 Pizzeria Codice e Basilico</span>
         </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+        <div class="d-lg-none d-flex justify-content-between mx-auto">
+          <ul class="navbar-nav  d-lg-none   me-5 fs-4 fw-bold">
+            <li class="nav-item ">
+              <a
+                class="nav-link position-relative "
+                routerLink="/carrello"
+                routerLinkActive="active"
+              >
+                Carrello
+                <i class="bi bi-cart"></i>
+
+                <span
+                  *ngIf="cartItemCount()"
+                  class="badge bg-danger rounded-pill position-absolute top-45 start-95 translate-middle"
+                  [class.d-none]="cartItemCount() === 0"
+                >
+                  {{ cartItemCount() }}
+                  <span class="visually-hidden">articoli nel carrello</span>
+                </span>
+              </a>
+            </li>
+          </ul>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon fs-4 "></span>
+          </button>
+        </div>
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-5 fs-4 fw-bold">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-5 fs-4 fw-bold gap-2">
             <li class="nav-item">
               <a
                 class="nav-link"
@@ -40,7 +63,7 @@ import { CommonModule } from '@angular/common';
                 >Menu</a
               >
             </li>
-            <li class="nav-item ">
+            <li class="nav-item d-sm-block d-none ">
               <a
                 class="nav-link position-relative "
                 routerLink="/carrello"
